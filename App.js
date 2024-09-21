dc7222fbfe2aa24a259cea78e9d9036471c589ab
roshidhmohammed <115979576+roshidhmohammed@users.noreply.github.com>
2024-09-21 21:53:26 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,31 @@
+{
+  /* <div id="parent">
+<div id="child">
+<h1 id="heading1">I am an h1 tag</h1>
+<h1 id="heading2">I am an h2 tag</h1>
+</div>
+
+<div id="child">
+<h1 id="heading1">I am an h1 tag</h1>
+<h1 id="heading2">I am an h2 tag</h1>
+</div>
+</div> */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {id:"heading1"}, "I am an h1 tag"),
+    React.createElement("h2", {id:"heading2"}, "I am an h2 tag"),
+  ]), React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {id:"heading1"}, "I am an h1 tag"),
+    React.createElement("h2", {id:"heading2"}, "I am an h2 tag"),
+  ])]
+);
+
+console.log(parent); //js object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
